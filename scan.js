5c421089b3413d05fa3f2d3b09d63f4dd5ab62db
Matheus Cardoso <83430258+oMatheusCardoso@users.noreply.github.com>
2023-01-20 09:34:45 -0300

    Atualizão Funcional - Pesquisa 1.21

--- scan.js
+++ scan.js
@@ -7,11 +7,18 @@
 
 
 
-function logout(){
-    firebase.auth().signOut().then(() => {
-        window.location.href = "index.html";
-    }).catch(() => {
-        alert('Erro ao fazer logout!')
-    });
-}
+// function logout(){
+//     firebase.auth().signOut().then(() => {
+//         window.location.href = "index.html";
+//     }).catch(() => {
+//         alert('Erro ao fazer logout!')
+//     });
+// }
 
+var input = document.getElementById("findID");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("find").click();
+  }
+});
